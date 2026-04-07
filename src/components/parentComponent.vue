5bbf6b2354f9c4315b73d6f5d82a206867f0b82a
Marc-E <marcelzinga@live.nl>
2026-04-07 12:26:40 +0200

    remove duplicate test

--- src/components/parentComponent.vue
+++ src/components/parentComponent.vue
@@ -1,11 +1,9 @@
 <template>
-    <childComponent :message="title" />
     <childComponent :message="message" />
 </template>
 
 <script setup>
 import childComponent from './childComponent.vue';
 
-const title = 'Welkom in Vue 3';
 const message = 'Hallo Vue 3!';
 </script>
