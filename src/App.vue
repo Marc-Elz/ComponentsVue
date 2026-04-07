93b95eff3f3a052d6fc520aa1479a7b00347d6f8
Marc-E <marcelzinga@live.nl>
2026-04-07 13:31:22 +0200

    Stap 2: defineModel 1-4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup>
-import parentComponent from './components/parentComponent.vue';
+import ModelParentComponent from './components/modelParentComponent.vue';
+import ParentComponent from './components/parentComponent.vue';
 </script>
 
 <template>
-    <parentComponent />
+    <p>Stap 1: Werken met defineProps en defineEmits</p>
+    <ParentComponent />
+
+    <b>Stap 2: Werken met defineModel</b>
+    <ModelParentComponent />
 </template>
---
+++ src/components/modelParentComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <modelChildComponent v-model="sharedMessage" />
+    <p>Huidig bericht: {{ sharedMessage }}</p>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+import modelChildComponent from './modelChildComponent.vue';
+
+const sharedMessage = ref('');
+</script>
